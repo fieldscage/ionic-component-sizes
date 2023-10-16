55bc20d0962c974a690744046bbe5c6be695b905
Fields Cage <dev@fieldscage.com>
2023-10-16 12:54:00 -0500

    Initial push

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <ion-app>
+    <ion-router-outlet />
+  </ion-app>
+</template>
+
+<script setup lang="ts">
+import { IonApp, IonRouterOutlet } from '@ionic/vue';
+</script>
+
+<style lang="css">
+html {
+    font-size: 10px;
+}
+
+body {
+    font-size: 1.6rem;
+}
+
+#example-size {
+    width: 12rem;
+    height: 12rem;
+    margin: 2rem auto;
+    background-color: var(--ion-color-secondary);
+    border-radius: 6rem;
+}
+</style>
